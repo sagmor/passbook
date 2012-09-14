ff04a2b51156b250025d6467704cb6947a785f53
Rianol Jou <ar3sti@gmail.com>
2012-09-14 10:53:00 +0800

    Add intermediate certificate support

--- lib/passbook.rb
+++ lib/passbook.rb
@@ -2,7 +2,7 @@ require "passbook/version"
 require "passbook/pkpass"
 
 module Passbook
-  mattr_accessor :p12_cert, :p12_password
+  mattr_accessor :p12_cert, :p12_password, :intermediate_cert
 
   def self.configure
     yield self
